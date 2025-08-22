89ffd4abb7df53b651b48f114919c5320edc0536
ebbryan <earlbryanburaay@gmail.com>
2025-08-22 10:37:03 +0800

    added mapping

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -9,7 +9,16 @@ const Todo = ({ todoData }: { todoData: TodoType[] }) => {
     <section>
       <div>
         <h1 className="text-2xl font-bold">Todo List</h1>
-        {/* Todo list items will go here */}
+        <div className="overflow-y-auto max-h-[calc(100vh-200px)]">
+          {todoData.map((todo) => (
+            <div key={todo.id} className="border p-4 mb-2">
+              <h2 className="text-xl">{todo.title}</h2>
+              <p>Status: {todo.status}</p>
+              <p>Created At: {new Date(todo.createdAt).toLocaleString()}</p>
+              <p>Updated At: {new Date(todo.updatedAt).toLocaleString()}</p>
+            </div>
+          ))}
+        </div>
       </div>
       <div>TODO FORM HERE</div>
     </section>
