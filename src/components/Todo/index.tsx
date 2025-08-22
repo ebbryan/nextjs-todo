99e9bc4c24ed48ac80c8664cddae8b240f3cf827
ebbryan <earlbryanburaay@gmail.com>
2025-08-22 14:35:03 +0800

    added onEditClick handlers

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,19 +1,62 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import TodoItem from "../TodoItem";
 import CreateForm from "../TodoForm/CreateForm";
 import { TodoType } from "@/zodTypes/todo.type";
 
+export type TodoUpdateData = Omit<TodoType, "createdAt" | "updatedAt">;
+
 const Todo = ({ todoData }: { todoData: TodoType[] }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [updateTodo, setUpdateTodo] = useState<TodoUpdateData | undefined>({
+    id: "",
+    title: "",
+    status: "pending",
+  });
+
+  const togglers = () => {
+    const onEdit = (todoPayload: TodoUpdateData | undefined) => {
+      if (!isEditing) setIsEditing(true);
+      setUpdateTodo(todoPayload);
+    };
+    const onCloseEdit = () => {
+      if (isEditing) setIsEditing(false);
+      setUpdateTodo(undefined);
+    };
+
+    return { onEdit, onCloseEdit };
+  };
+
+  const todoClickHandler = (todoPayload: TodoUpdateData | undefined) => {
+    if (todoPayload) {
+      togglers().onEdit(todoPayload);
+    }
+  };
+
+  const todoCloseHandler = () => {
+    togglers().onCloseEdit();
+  };
+
   return (
     <section className="w-full mx-auto p-2">
       <div className="overflow-y-auto max-h-[calc(100vh-200px)] [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
         {todoData.map((todo) => (
-          <TodoItem todo={todo} key={todo.id} />
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onTodoClick={(updateData) => todoClickHandler(updateData)}
+            onCloseEdit={todoCloseHandler}
+          />
         ))}
       </div>
-      <CreateForm />
+      {isEditing ? (
+        <h1>
+          {updateTodo?.title}, {updateTodo?.status}
+        </h1>
+      ) : (
+        <CreateForm />
+      )}
     </section>
   );
 };
--- src/components/TodoItem/index.tsx
+++ src/components/TodoItem/index.tsx
@@ -1,14 +1,29 @@
 import { TodoType } from "@/zodTypes/todo.type";
 import React from "react";
 import dayjs from "dayjs";
+import { TodoUpdateData } from "../Todo";
 
-const TodoItem = ({ todo }: { todo: TodoType }) => {
+const TodoItem = ({
+  todo,
+  onTodoClick,
+  onCloseEdit,
+}: {
+  todo: TodoType;
+  onTodoClick: (updateData: TodoUpdateData | undefined) => void;
+  onCloseEdit?: () => void;
+}) => {
   return (
     <div className="border p-4 mb-2">
       <h2 className="text-xl">{todo.title}</h2>
       <p>Status: {todo.status}</p>
       <p>Created At: {dayjs(todo.createdAt).format("MMM-DD-YYYY")}</p>
       <p>Updated At: {dayjs(todo.updatedAt).format("MMM-DD-YYYY")}</p>
+      <div className="cursor-pointer" onClick={() => onTodoClick(todo)}>
+        TEST ICON FOR EDIT MODE!
+      </div>
+      <div className="cursor-pointer" onClick={onCloseEdit}>
+        TEST ICON FOR Close Edit MODE!
+      </div>
     </div>
   );
 };
