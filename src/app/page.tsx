4cac854a240f7221b6bf647e2fe7c62636114dfe
ebbryan <earlbryanburaay@gmail.com>
2025-08-22 10:45:05 +0800

    added sorting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,5 +9,16 @@ export default async function Home() {
     return <div className="text-center">No todos available</div>;
   }
 
-  return <Todo todoData={data} />;
+  return (
+    <main>
+      <h1 className="text-3xl font-bold text-center my-2">Todo App</h1>
+      <p className="text-center">Manage your tasks efficiently</p>
+      <Todo
+        todoData={data.sort(
+          (a, b) =>
+            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+        )}
+      />
+    </main>
+  );
 }
--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -4,21 +4,17 @@ import { TodoType } from "@/zodTypes/todo.type";
 import React from "react";
 
 const Todo = ({ todoData }: { todoData: TodoType[] }) => {
-  console.log("Todo Data:", todoData);
   return (
     <section>
-      <div>
-        <h1 className="text-2xl font-bold">Todo List</h1>
-        <div className="overflow-y-auto max-h-[calc(100vh-200px)]">
-          {todoData.map((todo) => (
-            <div key={todo.id} className="border p-4 mb-2">
-              <h2 className="text-xl">{todo.title}</h2>
-              <p>Status: {todo.status}</p>
-              <p>Created At: {new Date(todo.createdAt).toLocaleString()}</p>
-              <p>Updated At: {new Date(todo.updatedAt).toLocaleString()}</p>
-            </div>
-          ))}
-        </div>
+      <div className="overflow-y-auto max-h-[calc(100vh-200px)]">
+        {todoData.map((todo) => (
+          <div key={todo.id} className="border p-4 mb-2">
+            <h2 className="text-xl">{todo.title}</h2>
+            <p>Status: {todo.status}</p>
+            <p>Created At: {new Date(todo.createdAt).toLocaleString()}</p>
+            <p>Updated At: {new Date(todo.updatedAt).toLocaleString()}</p>
+          </div>
+        ))}
       </div>
       <div>TODO FORM HERE</div>
     </section>
