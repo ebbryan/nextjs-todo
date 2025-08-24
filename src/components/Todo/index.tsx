959368ce7a8a2bdfb902ce8afc9a37a4e4677b3c
ebbryan <earlbryanburaay@gmail.com>
2025-08-24 11:26:50 +0800

    cleanup

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -57,7 +57,9 @@ const Todo = (props: TodoComponentProps) => {
     <section className="w-full mx-auto p-2 flex flex-col gap-4">
       <div className="flex flex-col gap-2 overflow-y-auto max-h-[calc(100vh-200px)] [scrollbar-width:none] [-ms-overflow-style:none] [&::-webkit-scrollbar]:hidden">
         {!props.isSuccess || !props.todoData || props.todoData.length === 0 ? (
-          <div className="text-center">No todos available</div>
+          <div className="text-center flex flex-col items-center justify-center max-h-[calc(100vh-200px)]">
+            <h1>No todos available</h1>
+          </div>
         ) : (
           props.todoData.map((todo) => (
             <TodoItem
